308d701cf97222dda939d43debb1098ac46be4da
0PaleJo0 <94214316+0PaleJo0@users.noreply.github.com>
2022-12-29 06:01:54 +0300

    Update ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://93.158.225.41:8000/web-data', {
+        fetch('http://localhost:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
